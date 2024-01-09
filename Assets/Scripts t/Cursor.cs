7a426f8bfd1f0d7ac8fe8eb899ab4b437709c2d5
Trymeren <tnblytt@gmail.com>
2024-01-09 22:34:28 +0100

    Building 2
    Tried to get the cursor accurate no matter the screen size, and kinda did it, but dont use anything other than a 16:9 ratio

--- Assets/Scripts t/Cursor.cs
+++ Assets/Scripts t/Cursor.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Cursor : MonoBehaviour
 {
-    [SerializeField] private Vector3 mousePos;
+    public Vector3 mousePos;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +14,7 @@ public class Test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mousePos = new Vector3((Input.mousePosition.x - Screen.width/2) / 13.5f, 0, (Input.mousePosition.y - Screen.height/2));
+        mousePos = GameObject.Find("Canvas").GetComponent<MainCanvas>().mousePos;
         transform.position = mousePos;
-
-        Debug.Log(mousePos);
     }
 }
--- Assets/Scripts t/MainCanvas.cs
+++ Assets/Scripts t/MainCanvas.cs
@@ -6,7 +6,9 @@ public class MainCanvas : MonoBehaviour
 {
     private int buildingSelected;
 
-    private Vector3 mousePos;
+    public Vector3 mousePos {get; private set;}
+
+    [SerializeField] private Vector3 MousePos;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +18,19 @@ public class MainCanvas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mousePos = Input.mousePosition;
+        CursorPos();
+        MousePos = mousePos;
     }
 
     public void SelectBuilding(int building)
     {
         buildingSelected = building;
+    }
+
+    void CursorPos()
+    {
+        mousePos = new Vector3((Input.mousePosition.x - Screen.width/2) / (((Screen.width / 46) / 15.93f) * 9), 0, (Input.mousePosition.y - Screen.height/2) / (Screen.height / 45.7f));
+        Debug.Log(Screen.height + " and: " + Screen.width);
     }
 
 }
