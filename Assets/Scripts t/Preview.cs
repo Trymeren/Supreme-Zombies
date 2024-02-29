1a816a2ecdff807348674f3c981832411efc6a2a
Trymeren <tnblytt@gmail.com>
2024-02-29 01:22:32 +0100

    Building Obstructions
    Made buildings not placable if they are touching an "obstacle"

--- Assets/Scripts t/Preview.cs
+++ Assets/Scripts t/Preview.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Preview : MonoBehaviour
 {
     private Vector3 posOffset = new Vector3(0, 2, 0);
     private MainCanvas mainCanvas;
+
+    [SerializeField] private int placeable = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,14 +23,34 @@ public class Preview : MonoBehaviour
         //Check for placement
         if(Input.GetKeyUp(KeyCode.Mouse0))
         {
-            StartCoroutine(PlaceStructure());
+            if(placeable == 0)
+            {
+                StartCoroutine(PlaceStructure());
+            }
+            
+        }
+    }
+
+    //Check if placeable
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.CompareTag("Building"))
+        {
+            placeable++;
+        }
+    }
+        
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Building"))
+        {
+            placeable--;
         }
     }
 
     IEnumerator PlaceStructure()
     {
         yield return new WaitForSeconds(Time.deltaTime * 3);
-        Debug.Log("Place");
 
         Instantiate(mainCanvas.buildingPrefabs[mainCanvas.buildingSelected - 1], transform.position, transform.rotation);
         Destroy(gameObject);
@@ -45,6 +68,14 @@ public class Preview : MonoBehaviour
 
     private void ChangeColour()
     {
-        gameObject.GetComponent<Renderer>().material.SetColor("_Color", new Color(0, 0.85f, 0, 0.8f));
+        if(placeable > 0)
+        {
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color", new Color(0.85f, 0, 0, 0.8f));
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color", new Color(0, 0.85f, 0, 0.8f));
+        }
+        
     }
 }
